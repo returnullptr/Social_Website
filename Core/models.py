e0dfdad638321fe8122e80a7e0190d37a587667a
Rocky Ren <yren21@icloud.com>
2023-01-08 11:23:30 -0800

    Initialization

--- Core/models.py
+++ Core/models.py
@@ -1,5 +1,63 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+import uuid
+from datetime import datetime
+
+User = get_user_model()
 
 # Create your models here.
 class Profile(models.Model):
+    # current user
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     id_user = models.IntegerField()
+
+    # personal statement
+    bio = models.TextField(blank=True)
+
+    # avatar
+    profile_img = models.ImageField(upload_to="profile_images", default="blank_profile_image.png")
+
+    # location
+    location = models.CharField(max_length=100, blank=True)
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+
+    # user id
+    user = models.CharField(max_length=100)
+
+    # image posted
+    image = models.ImageField(upload_to="post_images")
+
+    # post title
+    caption = models.TextField()
+    created_time = models.DateTimeField(default=datetime.now)
+
+    # likes
+    count_of_likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user
+
+class LikePost(models.Model):
+    # post id
+    post_id = models.CharField(max_length=500)
+
+    # user id
+    username = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
+
+class FollowerCount(models.Model):
+    # follower
+    follower = models.CharField(max_length=100)
+
+    # user
+    user = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user
+
+
+
--- Core/views.py
+++ Core/views.py
@@ -1,3 +1,72 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
+
+#
+def signup(request):
+    return
+
+#
+def signin(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # verify
+        user = auth.authenticate(username=username, password=password)
+
+        # if user exists
+        if user is not None:
+            messages.info(request, "Successfully Logged In")
+            return redirect('signin')
+        # if user does not exist
+        else:
+            messages.info(request, "Unable to Log In")
+            return redirect('signin')
+
+    else:
+        return render(request, 'signin.html')
+
+def signup(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # match passwords
+        if password == password2:
+            # if email already exist
+            if User.objects.filter(email=email).exists():
+                messages.info(request, "Email Taken")
+                return redirect('signup')
+
+            # if username already exist
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, "Username Taken")
+                return redirect('signup')
+
+            # if none exists
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                messages.info(request, "Successfully Signed Up")
+                return redirect('signup')
+
+        else:
+            messages.info(request, "Passwords Do Not Match")
+            return redirect('signup')
+
+    else:
+        return render(request, 'signup.html')
+
+
+#
+def settings(request):
+    if request.method == "POST":
+        return redirect(settings)
+
+    else:
+        return render(request, 'settings.html')
